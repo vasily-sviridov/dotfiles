deb8988202f4012089dccc54e0766139cb06e59f
Vasily Sviridov <sviridovcomp@gmail.com>
2026-05-11 20:04:59 +0300

    feat: disable autocomplete open

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,10 +1,8 @@
 return {
-  {
-    "saghen/blink.cmp",
-    opts = function (_, opts)
-      opts.signature = opts.signature or {}
-      opts.signature.enabled = true
-      opts.signature.window = opts.signature.window or {}
-    end
-  }
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      menu = { auto_show = false },
+    },
+  },
 }
